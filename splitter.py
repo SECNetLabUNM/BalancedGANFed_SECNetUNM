8fda84fee9b8270ad7471387b09b98acb82474e5
yie-sheng chen <tallpik3@unm.edu>
2025-03-19 03:04:40 -0600

    8f318847-lobogit

---
+++ splitter.py
@@ -0,0 +1,34 @@
+import molecular_dataset_test
+from rdkit import Chem
+
+# 3\Documents\unm2h\git\graphganfeddrugbank\molecularGAN\GraphGANFed\molecular_dataset_merger.py
+def getAtomicNumRepresentative(filenames = ['data-smiles/qm8_smiles.pkl','data_smiles/drugbank/diabetes-drugbank.pkl']) :
+  fieldsList, fieldsDict, fieldsNameDict, filenameMDsDict, filenameMDsObjDict = molecular_dataset_test.getDicts()
+  smilesList = []
+  mols = []
+  atomicNumRepresentative = {} # atom.GetAtomicNum()
+  filenames = ['data-smiles/qm8_smiles.pkl','data_smiles/drugbank/diabetes-drugbank.pkl']
+  for filename in filenames:
+    mdata = filenameMDsObjDict[filename]
+    for mol in mdata.data:
+      if mol.GetNumAtoms() < 33:
+        mols.append(mol)
+        mSmil = Chem.CanonSmiles( Chem.MolToSmiles(mol) )
+        smilesList.append( mSmil )
+        for atom in mol.GetAtoms():
+          an = atom.GetAtomicNum()
+          # print( "an = atom.GetAtomicNum()", type( an ) )
+          if an not in atomicNumRepresentative:
+            atomicNumRepresentative[an] = mSmil
+
+  # print( "len(smilesList)", len(smilesList) )
+  # print( "atomicNumRepresentative", atomicNumRepresentative )
+  # print( "atomicNumRepresentative", len(atomicNumRepresentative) )
+  return atomicNumRepresentative
+
+if __name__ == '__main__':
+  import pprint
+  atomicNumRepresentative = getAtomicNumRepresentative()
+  print( "atomicNumRepresentative", len(atomicNumRepresentative) )
+  print( pprint.pformat(atomicNumRepresentative, sort_dicts=False) )
+
