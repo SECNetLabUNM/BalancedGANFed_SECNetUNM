9d2bff483dfb8761ff675b4d94860b7c6b6147b1
yie-sheng chen <tallpik3@unm.edu>
2025-03-19 03:49:19 -0600

    comment out filenames

--- molecular_dataset_test.py
+++ molecular_dataset_test.py
@@ -97,10 +97,10 @@ def getDicts():
     return fieldsList, fieldsDict, fieldsNameDict, filenameMDsDict, filenameMDsObjDict
 
 filenames = [
-  'data-smiles/qm8_smiles.pkl',
-  'data-smiles/qm9_smiles.pkl',
-  'data-smiles/esol_smiles.pkl',
-  'data_smiles/drugbank/diabetes-drugbank.pkl',
+  # 'data-smiles/qm8_smiles.pkl',
+  # 'data-smiles/qm9_smiles.pkl',
+  # 'data-smiles/esol_smiles.pkl',
+  # 'data_smiles/drugbank/diabetes-drugbank.pkl',
   'data_smiles/qm8-diabetes-drugbank.pkl',
 ]
 
